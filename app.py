01b4235e0e0379ac6af66943f7b29875af4dc138
Jainesh Sanghavi <jaineshs301email@gmail.com>
2026-08-25 23:19:18 +0530

    Fix Flask deployment paths

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import joblib
 
 app = Flask(__name__)
 
-model = joblib.load("model/supply_chain_model.pkl")
+model = joblib.load("../model/supply_chain_model.pkl")
 
 features = [
     "Average_Daily_Sales",
